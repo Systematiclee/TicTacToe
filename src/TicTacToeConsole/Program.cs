7d5d703828c05b1f811259cf180a146aabbf08fa
arooks2 <arooks2@jeffersonstate.edu>
2023-02-13 21:42:07 -0600

    User and system symbols along with gameboard

--- src/TicTacToeConsole/Program.cs
+++ src/TicTacToeConsole/Program.cs
@@ -10,6 +10,34 @@ namespace TicTacToeConsole
     {
         static void Main(string[] args)
         {
+            char userSymbol = 'X';
+            char systemSymbol = 'O';
+            char[,] gameBoard = new char[3, 3];
+
+            Console.Write($"You will be playing as X. ");
+            Console.WriteLine($"The system will be playing as O.\n");
+
+            DisplayGameBoard(gameBoard);
+
+            Console.WriteLine("Choose a row and column to place your symbol.");
+
+
+
+        }
+
+        static void DisplayGameBoard(char[,] gameBoard)
+        {
+            Console.WriteLine("Current Game Board:");
+            Console.WriteLine("   0  1  2");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"{i} ");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"[{gameBoard[i, j]}]");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
