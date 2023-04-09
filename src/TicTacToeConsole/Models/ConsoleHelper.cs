7d94d9d0e9a338a10b0799e75da4bdf81a742773
arooks2 <arooks2@jeffersonstate.edu>
2023-04-09 14:37:33 -0500

    Added comments to the code as well as a system for displaying game statistics

--- src/TicTacToeConsole/Models/ConsoleHelper.cs
+++ src/TicTacToeConsole/Models/ConsoleHelper.cs
@@ -4,29 +4,43 @@ namespace TicTacToeConsole.src.Models
 {
     public static class ConsoleHelper
     {
-
-        public static void DisplayGameBoard(char[,] gameBoard)
+        // A method that displays the current game board in the console.
+        // It takes a 2D char array representing the game board as a parameter.
+        public static void DisplayGameBoard(Game game, char[,] gameBoard)
         {
+            // Print user and system symbols
+            PrintLine($"User: {game.User.Symbol} | System: {game.System.Symbol}");
+
+            // Print column numbers
+            PrintLine("  0   1   2");
+
+            // Loop through each row of the game board.
             for (int row = 0; row < 3; row++)
             {
+                // Print row number
+                Print($"{row} ");
+
+                // Loop through each column of the game board.
                 for (int col = 0; col < 3; col++)
                 {
+                    // Get the value of the cell at the current row and column.
                     char cell = gameBoard[row, col];
-                    Console.Write(cell == '\0' ? " - " : $" {cell} ");
+                    Print(cell == '\0' ? " - " : $" {cell} ");
 
                     if (col < 2)
                     {
-                        Console.Write("|");
+                        Print("|");
                     }
                 }
 
                 if (row < 2)
                 {
-                    Console.WriteLine("\n-----------");
+                    PrintLine("\n  -----------");
                 }
             }
 
-            Console.WriteLine();
+            // Print an empty string to move the cursor to the next line.
+            PrintLine("");
         }
         public static void Print(string message)
         {
